cbbb0fc37b66ce589bc9673018e4ac5eea3ad1a7
Wenxin Wang <i@wenxinwang.me>
2026-09-06 23:54:41 +0800

    guix: channels lock

---
+++ env/guix/uraj/channels/channels-lock.scm
@@ -0,0 +1,10 @@
+(list (channel
+       (name 'guix)
+       (url "https://mirror.nju.edu.cn/git/guix.git")
+       (branch "master")
+       (commit "ede0b3fda5934119d8f2341a18d2837ee0ac1c43")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
