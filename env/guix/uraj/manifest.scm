d3ea135045949b729ec9b94399da9d504e84eae5
Wenxin Wang <i@wenxinwang.me>
2026-09-11 11:25:22 +0800

    guix: remove currently unused dependencies from manifest
    Somehow `guix` development inputs include `git`, which
    relies on the profile's certificate, which in turns relies on
    `nsscert`, and I think this is too complicated.
    
    Does this mean that even on guix os, I need to include
    `nsscert` in the manifest?

--- env/guix/uraj/manifest.scm
+++ env/guix/uraj/manifest.scm
@@ -2,21 +2,8 @@
              (guix profiles)
              (gnu packages))
 
-(define (specifications->development-manifest specs)
-  (let ((specification->development-manifest
-         (compose package->development-manifest
-                  specification->package)))
-    (concatenate-manifests
-     (map specification->development-manifest specs))))
-
 (concatenate-manifests
  (list (specifications->manifest
         (list "gnupg"
               "maak"
-              "sops"
-              ;; For Guix System installer.
-              "guile-newt"
-              "guile-parted"
-              "guile-webutils"))
-       (specifications->development-manifest
-        (list "guix"))))
+              "sops"))))
