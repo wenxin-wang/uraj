48e936968c08cae089fa75d0c2177b3f71d0cc8f
Wenxin Wang <i@wenxinwang.me>
2026-09-12 15:43:42 +0800

    guix: add my fork for testing own fixes

--- env/guix/uraj/manifest.scm
+++ env/guix/uraj/manifest.scm
@@ -2,8 +2,25 @@
              (guix profiles)
              (gnu packages))
 
+(define (specifications->development-manifest specs)
+  (let ((specification->development-manifest
+         (compose package->development-manifest
+                  specification->package)))
+    (concatenate-manifests
+     (map specification->development-manifest specs))))
+
 (concatenate-manifests
  (list (specifications->manifest
         (list "gnupg"
               "maak"
-              "sops"))))
+	      ;; `guix` development-manifest introduces `git`, and thus the
+	      ;; need for `nss-certs` for certificates on installations without
+	      ;; guix-os/guix-home.
+              "nss-certs"
+              "sops"
+              ;; For Guix System installer.
+              "guile-newt"
+              "guile-parted"
+              "guile-webutils"))
+       (specifications->development-manifest
+        (list "guix"))))
--- env/guix/uraj/os/maak.scm
+++ env/guix/uraj/os/maak.scm
@@ -1,5 +1,6 @@
 (define-module (maak)
   #:declarative? #t
+  #:use-module (guix build utils)
   #:use-module (maak dsl)
   #:use-module (maak maak)
   #:use-module (ice-9 match)
@@ -10,7 +11,7 @@
   #:use-module (srfi srfi-19)
   #:use-module (srfi srfi-26)
   #:use-module (uraj desktop env)
-  #:export (update-channels-lock guix-time-machine home-container))
+  #:export (update-channels-lock guix guix-my compile-guix home-container))
 
 (define project-root
   (canonicalize-path (string-append (dirname (current-filename)) "/../../../..")))
@@ -18,19 +19,38 @@
 (define (project-path relative)
   (string-append project-root "/" relative))
 
+(define (guix-uraj-path relative)
+  (project-path (string-append "env/guix/uraj/" relative)))
+
 (define (update-channels-lock)
-  (let ((tmp-output-filename (project-path "env/guix/uraj/channels-lock.scm.tmp")))
+  (let ((tmp-output-filename (guix-uraj-path "channels-lock.scm.tmp")))
     (with-output-to-file tmp-output-filename
       (lambda ()
         (time-machine '("describe" "-f" "channels")
-                      #:channels (project-path "env/guix/uraj/channels.scm"))))
+                      #:channels (guix-uraj-path "channels.scm"))))
     (unless (dry-run?)
-      (rename-file tmp-output-filename (project-path "env/guix/uraj/channels-lock.scm")))))
+      (rename-file tmp-output-filename (guix-uraj-path "channels-lock.scm")))))
 
-(define (guix-time-machine . cmd)
-  (time-machine cmd #:channels (project-path "env/guix/uraj/channels-lock.scm")))
+(define* ($guix args #:key (fork? #f))
+  (if fork?
+      ($ `(,(guix-uraj-path "pre-inst-env") "guix" ,@args))
+      (time-machine args #:channels (guix-uraj-path "channels-lock.scm"))))
 
-(define* (home-container #:key (config-path (project-path "env/guix/uraj/os/home-example.scm"))
+(define* (guix . args)
+  ($guix args))
+
+(define* (guix-my . args)
+  ($guix args #:fork? #t))
+
+(define (compile-guix)
+  ($ '("git" "submodule" "update" "--init"))
+  (with-directory-excursion (guix-uraj-path "channels/guix")
+    (unless (file-exists? "Makefile")
+      ($ '("./bootstrap"))
+      ($ '("./configure")))
+    ($ `("make" "-j" ,(number->string (current-processor-count))))))
+
+(define* (home-container #:key (config-path (guix-uraj-path "os/home-example.scm"))
                          (command '()))
   "Run the home environment in a container, sharing the host's display and audio."
   (match (desktop-container-envs)
@@ -43,4 +63,4 @@
             (tail (cond ((pair? env)    `("--" ,script ,@command))
                         ((pair? command) `("--" ,@command))
                         (else            '()))))
-       (apply guix-time-machine (append head tail))))))
+       ($guix (append head tail))))))
